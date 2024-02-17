ebd589245e2361c790cac003419d4a94705f459e
Sakshi Swarup <85953435+sakshiswarup@users.noreply.github.com>
2024-02-17 20:10:38 +0530

    Add files via upload

---
+++ DuplicateElementInArray.java
@@ -0,0 +1,19 @@
+package java_1;
+//Duplicate element in array
+public class DuplicateElementInArray {
+	public static void main(String[] args) {
+		int[] x = { 1, 1, 2, 2, 3, 3, 4, 5, 6 ,6 }; //we can also write int x[]={}
+		int[] temp = new int[x.length];
+		int j = 0;
+		for (int i = 0; i < x.length - 1; i++) {
+			if (x[i] != x[i + 1]) {
+				temp[j] = x[i];
+				j++;
+			}
+		}
+		temp[j] = x[x.length - 1];
+		for (int z : temp) {
+			System.out.println(z);
+		}
+	}
+}
