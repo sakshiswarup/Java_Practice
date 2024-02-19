0d17ab931d15dcc8581b668010e98a7c030c0817
Sakshi Swarup <85953435+sakshiswarup@users.noreply.github.com>
2024-02-19 22:33:05 +0530

    Add files via upload

---
+++ Swapping_Array.java
@@ -0,0 +1,22 @@
+package java_1;
+
+public class Swapping_Array {
+	public static void main(String[] args) {
+		int []arr = {34,14,24,40,35};
+		int temp;
+		for(int j =0; j<arr.length-1;j++) {
+			for(int i =0; i<arr.length-1;i++) {
+				if(arr[i]>arr[i+1]) {
+					temp=arr[i];
+					arr[i]=arr[i+1];
+					arr[i+1]=temp;
+				}
+				
+			}
+		}
+		for(int x:arr) {
+			System.out.println(x);
+		}
+	}
+
+}
