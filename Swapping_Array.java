e18657fd1afbb8cf4e39234165585a4355e50549
Sakshi Swarup <85953435+sakshiswarup@users.noreply.github.com>
2024-02-21 19:00:51 +0530

    Swapping_Array.java

--- Swapping_Array.java
+++ Swapping_Array.java
@@ -1,22 +1,27 @@
 package java_1;
-//Swapping an Array
+
 public class Swapping_Array {
 	public static void main(String[] args) {
-		int []arr = {34,14,24,40,35};
+		int[] arr = { 34, 14, 24, 40, 35 };
 		int temp;
-		for(int j =0; j<arr.length-1;j++) {
-			for(int i =0; i<arr.length-1;i++) {
-				if(arr[i]>arr[i+1]) {
-					temp=arr[i];
-					arr[i]=arr[i+1];
-					arr[i+1]=temp;
+		for (int j = 0; j < arr.length - 1; j++) {
+			for (int i = 0; i < arr.length - 1; i++) {
+				if (arr[i] > arr[i + 1]) {
+					temp = arr[i];
+					arr[i] = arr[i + 1];
+					arr[i + 1] = temp;
 				}
-				
+
 			}
 		}
-		for(int x:arr) {
+		for (int x : arr) {
 			System.out.println(x);
 		}
+		System.out.println(arr[arr.length - 1]);
+		System.out.println(arr[arr.length - 2]);
+		
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
 	}
 
 }
