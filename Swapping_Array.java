955621db10b2cb097869e07b5ac2f93763bfe305
Sakshi Swarup <85953435+sakshiswarup@users.noreply.github.com>
2024-02-22 19:25:42 +0530

    Swapping_Array.java

--- Swapping_Array.java
+++ Swapping_Array.java
@@ -2,7 +2,7 @@ package java_1;
 
 public class Swapping_Array {
 	public static void main(String[] args) {
-		int[] arr = { 34, 14, 24, 40, 35 };
+		int[] arr = { 54, 16, 24, 40, 35 };
 		int temp;
 		for (int j = 0; j < arr.length - 1; j++) {
 			for (int i = 0; i < arr.length - 1; i++) {
