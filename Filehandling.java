52fe88e0d102028de89e998876afe674f3b5a59b
Sakshi Swarup <85953435+sakshiswarup@users.noreply.github.com>
2024-02-22 19:39:01 +0530

    Filehandling.java

--- Filehandling.java
+++ Filehandling.java
@@ -1,5 +1,6 @@
 package java_1;
-// exists() - checks whether the file is present in given path. if yes this will return true or else false
+// exists() - it is a non static method persent inside file class.
+//checks whether the file is present in given path. if yes this will return true or else false
 import java.io.File;
 
 public class Filehandling {
@@ -7,6 +8,18 @@ public class Filehandling {
 public static void main(String[] args) {
 	File f = new File("G://hello//A.txt");
 	 boolean val = f.exists();
+	 System.out.println(val);
+}
+}
+
+//delete()- it is a non static method persent inside file class.
+//it will delete a file in the given path. If the file is not deleted it will return false
+//if file is deleted then it will return true
+public class Filehandling {
+
+public static void main(String[] args) {
+	File f = new File("G://hello//A.txt");
+	   boolean val=f.delete();
 	 System.out.println(val);
 }
 }
