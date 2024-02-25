e66afc0f0d363f086f058a8206cc9ffaa42c1455
Sakshi Swarup <85953435+sakshiswarup@users.noreply.github.com>
2024-02-25 16:49:40 +0530

    Add files via upload

--- Filehandling.java
+++ Filehandling.java
@@ -1,20 +1,23 @@
 package java_1;
-// exists() - it is a non static method persent inside file class.
+// exists() -  it is a non static method persent inside file class.
 //checks whether the file is present in given path. if yes this will return true or else false
 import java.io.File;
 
-public class Filehandling {
 
-public static void main(String[] args) {
-	File f = new File("G://hello//A.txt");
-	 boolean val = f.exists();
-	 System.out.println(val);
-}
-}
+
+//public class Filehandling {
+//
+//public static void main(String[] args) {
+//	File f = new File("G://hello//A.txt");
+//	 boolean val = f.exists();
+//	 System.out.println(val);
+//}
+//}
 
 //delete()- it is a non static method persent inside file class.
 //it will delete a file in the given path. If the file is not deleted it will return false
 //if file is deleted then it will return true
+
 public class Filehandling {
 
 public static void main(String[] args) {
@@ -23,3 +26,5 @@ public static void main(String[] args) {
 	 System.out.println(val);
 }
 }
+
+
