50ce5f994c1bf5b01f8e746f66b45b1d3fb87764
Sakshi Swarup <85953435+sakshiswarup@users.noreply.github.com>
2024-02-26 18:30:46 +0530

    Add files via upload

---
+++ Str1.java
@@ -0,0 +1,11 @@
+package string_java;
+//Reverse String
+public class Str1 {
+	public static void main(String[] args) {
+		String str ="Sakshi Swarup";
+		for(int i = str.length()-1; i>0;i--) {
+			System.out.println(str.charAt(i));
+		}
+	}
+}
+
