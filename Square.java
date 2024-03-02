7b98e8d49132a00524b514a19da4bb2ab869ffd5
Sakshi Swarup <85953435+sakshiswarup@users.noreply.github.com>
2024-03-02 20:13:40 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,31 @@
+package string_java;
+//star square
+
+//public class Square {
+//	public static void main(String[] args) {
+//		for(int i=0;i<5;i++) {
+//			for(int j=0;j<5;j++) {
+//				System.out.print("*");
+//			}
+//		System.out.print("\n");}
+//	}
+//}
+//
+
+//triangle shape
+public class Square {
+	public static void main(String[] args) {
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				if (i == 0 && j == 1 || i == 0 && j == 2 || i == 0 && j == 3 || i == 0 && j == 4 || i == 1 && j == 2
+						|| i == 1 && j == 3 || i == 1 && j == 4 || i == 2 && j == 3 || i == 2 && j == 4
+						|| i == 3 && j == 4) {
+					System.out.print(" ");
+				} else {
+					System.out.print("*");
+				}
+			}
+			System.out.print("\n"); //or System.out.println();
+		}
+	}
+}
