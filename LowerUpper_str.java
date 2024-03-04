5a8f55512d60abf7c12cd44f491d559d1a3994f4
Sakshi Swarup <85953435+sakshiswarup@users.noreply.github.com>
2024-03-04 19:39:26 +0530

    Add files via upload

---
+++ LowerUpper_str.java
@@ -0,0 +1,18 @@
+package string_java;
+
+//public class LowerUpper_str {
+//public static void main(String[] args) {
+//	String str = "SakshiSwarup";
+//	System.out.println(str.toLowerCase());
+//	System.out.println(str.toUpperCase());
+//}
+//}
+
+//Remove Whitespace in string
+public class LowerUpper_str {
+public static void main(String[] args) {
+	String str = " SakshiSwarup ";
+	System.out.println(str);
+	System.out.println(str.trim());
+}
+}
