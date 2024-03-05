db547cc99146be9ea445cc8cf366e34aba6d2e90
Sakshi Swarup <85953435+sakshiswarup@users.noreply.github.com>
2024-03-05 19:13:38 +0530

    Add files via upload

---
+++ String/Str4.java
@@ -0,0 +1,20 @@
+package string_java;
+//Start with a & end with b;
+
+//public class Str4 {
+//public static void main(String[] args) {
+//	String str ="aabaaabaaabaz";
+//	System.out.println(str.startsWith("a"));
+//	System.out.println(str.endsWith("z"));
+//	System.out.println(str.startsWith("x"));
+//	System.out.println(str.endsWith("x"));
+//}
+//}
+
+//Length of String
+public class Str4{
+	public static void main(String[] args) {
+		String str ="aabaabaaabaz";
+		System.out.println(str.length());//12
+	}
+}
