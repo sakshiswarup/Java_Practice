829502df9292252b72c0aa122c86115c261038a0
Sakshi Swarup <85953435+sakshiswarup@users.noreply.github.com>
2024-03-06 19:57:01 +0530

    Str5.java

--- String/Str5.java
+++ String/Str5.java
@@ -1,23 +1,24 @@
 package string_java;
 // How value of method converts even type such as int, long, float, double & char array to string.
 
-//final public class Str5 {
-//public static void main(String[] args) {
-//	char[] i = {'a','b','c'}; 
-//	String str = String.valueOf(i);
-//	System.out.println(str); output abc
-//}
-//}
+final public class Str5 {
+public static void main(String[] args) {
+	char[] i = {'a','b','c'}; 
+	String str = String.valueOf(i);
+	System.out.println(str); output abc
+}
+}
 
+//for boolean
+final public class Str5 {
+public static void main(String[] args) {
+	boolean i = true; //output true
+	String str = String.valueOf(i);
+	System.out.println(str);
+}
+}
 
-//final public class Str5 {
-//public static void main(String[] args) {
-//	boolean i = true; //output true
-//	String str = String.valueOf(i);
-//	System.out.println(str);
-//}
-//}
-
+//for int
 final public class Str5 {
 public static void main(String[] args) {
 	int i = 10;//output 10
