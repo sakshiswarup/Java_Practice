c2ac1f7cfcf6d324acc17b2fe0a4bbaaf228e792
Sakshi Swarup <85953435+sakshiswarup@users.noreply.github.com>
2024-03-16 22:31:45 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,20 @@
+package thread_example;
+
+//Thread-multitasking done at the program level purpose is called as threads.
+//the main thread is improved the performance of the application by reducing excecution time
+public class A extends Thread {// two methods present in threads-run(),start()
+	@Override
+	public void run() {
+		for (int i = 0; i < 1000; i++) {
+			System.out.println("run");
+		}
+	}
+
+	public static void main(String[] args) {
+		A a1 = new A();
+		a1.start();
+		for (int i = 0; i < 1000; i++) {
+			System.out.println("main");
+		}
+	}
+}
