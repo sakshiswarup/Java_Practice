ef1080ba896294f84f92fe61b647e88113e6f6b3
Sakshi Swarup <85953435+sakshiswarup@users.noreply.github.com>
2024-03-18 21:32:47 +0530

    A.java

--- Threads/A.java
+++ Threads/A.java
@@ -2,7 +2,7 @@ package thread_example;
 
 //Thread-multitasking done at the program level purpose is called as threads.
 //the main thread is improved the performance of the application by reducing excecution time
-public class A extends Thread {// two methods present in threads-run(),start()
+public class A extends Thread {// two methods present in threads-> run(),start()
 	@Override
 	public void run() {
 		for (int i = 0; i < 1000; i++) {
@@ -18,3 +18,20 @@ public class A extends Thread {// two methods present in threads-run(),start()
 		}
 	}
 }
+
+
+//creating multithreads. 
+public class A extends Thread {
+	
+	String name;
+	A(String name){
+		this.name = name;
+	}
+@Override
+	public void run() {
+		for(int i = 0; i<10000; i++) {
+			System.out.println(this.name);
+		}
+	}
+	}
+//one more part in B.java
