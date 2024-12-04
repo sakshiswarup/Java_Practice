3f124e56fb16eeb437dd14bf6afc934bb9554843
Sakshi Swarup <85953435+sakshiswarup@users.noreply.github.com>
2024-12-04 23:07:18 +0530

    Add files via upload

---
+++ Def/Cons.java
@@ -0,0 +1,13 @@
+package Constructor.Def;
+//Default Constructor-
+//Whenever  we create abject it is mandatory to call constructor , If abject is created and constructor  is not developed
+// explicitly inside .java file then during compilation automatically  empty body No Args constructor gets added in .class file
+//This constructor is called as Default constructor & hence we  do not see any error during object creation.
+public class Cons {
+    //no constructor
+    public static void main(String[] args) {
+        Cons cons = new Cons();
+
+    }
+
+}
