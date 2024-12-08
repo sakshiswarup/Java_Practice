4047ab334fb24fac09fa850f2bf68fcb8d161d57
Sakshi Swarup <85953435+sakshiswarup@users.noreply.github.com>
2024-12-08 22:06:00 +0530

    Add files via upload

---
+++ privatep1/B.java
@@ -0,0 +1,23 @@
+package privatep1;
+
+//public class B extends A {
+// public static void main(String[] args) {
+//	B b1 = new B();
+//	System.out.println(b1.x); //error
+//	//b1.test();//error
+//}
+//}
+
+// private-same package non sub class
+//public class B{
+//	public static void main(String[] args) {
+//		B b1 = new B();
+//		System.out.println(b1.x);// error
+//		b1.test();//error
+//	}
+//}
+public class B{
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ privatep2/C.java
@@ -0,0 +1,20 @@
+package privatep2;
+
+//import privatep1.A;
+//
+//public class C extends A {
+//public static void main(String[] args) {
+//	C c1 = new C();
+//	//System.out.println(c1.x);//error
+//	//c1.test();//error
+//}
+//}
+
+//different package non sub class
+public class C  {
+public static void main(String[] args) {
+	C c1 = new C();
+	//System.out.println(c1.x);//error
+	//c1.test();//error
+}
+}
