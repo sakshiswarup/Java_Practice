ed190cb653a1eeccef0b4073403c4136bca83b8d
Sakshi Swarup <85953435+sakshiswarup@users.noreply.github.com>
2024-12-09 22:09:48 +0530

    C.java

--- defaultp2/C.java
+++ defaultp2/C.java
@@ -1,20 +1,20 @@
 package defaultp2;
 
 import defaultp1.A;
-//default different package sub class- no
-//public class C extends A{
-// public static void main(String[] args) {
-//	C c1 = new C();
-//	c1.test();//error
-//	System.out.println(c1.x);//error
-//}
-//}
+default different package sub class- no
+public class C extends A{
+public static void main(String[] args) {
+	C c1 = new C();
+	c1.test();//error
+	System.out.println(c1.x);//error
+}
+}
 
 //default different package non sub class= no
-//public class C{
-//	 public static void main(String[] args) {
-//		C c1 = new C();
-//		c1.test();//error
-//		System.out.println(c1.x);//error
-//	}
-//	}
+public class C{
+	 public static void main(String[] args) {
+		C c1 = new C();
+		c1.test();//error
+		System.out.println(c1.x);//error
+	}
+	}
