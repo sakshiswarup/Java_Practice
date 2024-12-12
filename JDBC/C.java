2c0feed1561183cc95c34f2ec476ab77bc82f0e6
Sakshi Swarup <85953435+sakshiswarup@users.noreply.github.com>
2024-12-12 16:49:38 +0530

    C.java

--- JDBC/C.java
+++ JDBC/C.java
@@ -17,3 +17,18 @@ public static void main(String[] args) {
 
 }
 }
+//-------------------------------------------------------------------------------------------------------
+public class Update {
+ public static void main(String[] args) {
+	try {
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db","root","test");
+		System.out.println(con);
+		
+		Statement stmnt = con.createStatement();
+		stmnt.executeUpdate("UPDATE registration SET mobile='9379845650', email='mikethyson@gmail.com' WHERE email='mike@gmail.com'");
+		con.close();
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+}
+}
