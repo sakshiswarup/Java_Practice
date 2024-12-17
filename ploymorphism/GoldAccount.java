510062129b4cc8edcc36da9299371bfe128d95bb
Sakshi Swarup <85953435+sakshiswarup@users.noreply.github.com>
2024-12-17 21:17:44 +0530

    GoldAccount.java

--- ploymorphism/GoldAccount.java
+++ ploymorphism/GoldAccount.java
@@ -8,3 +8,13 @@ public void rateOfInterest() {
 	System.out.println("nill");
 }
 }
+
+//for error because not same method inherited in PlatinumAccount
+public class GoldAccount {
+public void onlineBanking() {
+	System.out.println("yes");
+}
+public void rateOfInterest() {
+	System.out.println("Nill");
+}
+}
