1063f4a2351bf925f71bef2ca40c07dfaa742ff1
Sakshi Swarup <85953435+sakshiswarup@users.noreply.github.com>
2024-12-17 21:18:30 +0530

    PlatinumAccount.java

--- ploymorphism/PlatinumAccount.java
+++ ploymorphism/PlatinumAccount.java
@@ -16,3 +16,22 @@ public static void main(String[] args) {
 	
 }
 }
+
+public class PlatinumAccount extends GoldAccount{
+	public void onlineBanking() {
+		System.out.println("yes");
+	}
+	@Override
+	public int rateOfInterest() {//error because of not same method as previous.
+		System.out.println("6%");
+	}
+	public static void main(String[] args) {
+		GoldAccount g = new GoldAccount();
+		g.onlineBanking();
+		g.rateOfInterest();
+		
+		PlatinumAccount p = new PlatinumAccount();
+		p.onlineBanking();
+		p.rateOfInterest()
+	}
+}
