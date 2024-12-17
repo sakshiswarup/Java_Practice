c7d6dd24740647d8fda74ffc2e973d060a0793f6
Sakshi Swarup <85953435+sakshiswarup@users.noreply.github.com>
2024-12-17 08:41:32 +0530

    Cat.java

--- ploymorphism/Cat.java
+++ ploymorphism/Cat.java
@@ -12,3 +12,17 @@ public static void main(String[] args) {
 	c1.noise();
 }
 }
+
+//@OverRidding annotation helps us to check Whether overriding is happening or not. if not then throw errors. 
+//public class Cat extends Dog {
+//	@Override
+//	//public void noise(){//error
+//		System.out.println("bowww...");
+//	}
+//public static void main(String[] args) {
+//	Cat c1 =new Cat();
+//	c1.eat();
+//	c1.walk();
+//	c1.noise();
+//}
+//}
