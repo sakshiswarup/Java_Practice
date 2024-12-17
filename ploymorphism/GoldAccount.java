2ac513995305ed05a8441f0b61b914c0ed92defa
Sakshi Swarup <85953435+sakshiswarup@users.noreply.github.com>
2024-12-17 21:07:01 +0530

    Add files via upload

---
+++ ploymorphism/GoldAccount.java
@@ -0,0 +1,10 @@
+package ploymorphism;
+
+public class GoldAccount {
+public void onlineBanking() {
+	System.out.println("yes");
+}
+public void rateOfInterest() {
+	System.out.println("nill");
+}
+}
