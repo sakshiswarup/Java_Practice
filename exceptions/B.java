770fcf1894d7a5634d807c7ce4b4a429f0755eec
Sakshi Swarup <85953435+sakshiswarup@users.noreply.github.com>
2024-12-18 22:33:29 +0530

    Add files via upload

---
+++ exceptions/B.java
@@ -0,0 +1,12 @@
+package exceptions;
+//Type Of Exceptions.
+//CompileTime / Checked Exception
+//these exception occurs during compilation phase of the project,that is when the .java file converted into .class file.
+//only example purpose this code is wrong
+//import java.io.*;
+//import io.fileReader;
+//public class B {
+// public static void main(String[] args) {
+//	fileReader fr = new fileReader("G://test/.text");//FileNotFound exception.
+//}
+//}
