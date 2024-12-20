a7f625f4e18216c999cebabb0f5947f2acb5ba46
Sakshi Swarup <85953435+sakshiswarup@users.noreply.github.com>
2024-12-20 20:48:45 +0530

    Scanner2.java

--- ScannerClass/Scanner2.java
+++ ScannerClass/Scanner2.java
@@ -2,43 +2,43 @@ package ScannerClass;
 
 import java.util.Scanner;
 
-//public class Scanner2 {
-//public static void main(String[] args) {
-//	Scanner scan = new Scanner(System.in);
-//	System.out.println("Enter your Employee Id:");
-//	Short empId = scan.nextShort();
-//	System.out.println("Your employee id is "+ empId);
-//}
-//}
+public class Scanner2 {
+public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	System.out.println("Enter your Employee Id:");
+	Short empId = scan.nextShort();
+	System.out.println("Your employee id is "+ empId);
+}
+}
 
-//public class Scanner2{
-//	public static void main(String[] args) {
-//		Scanner scan = new Scanner(System.in);
-//		System.out.println("Enter your Employee Id:");
-//		Long empId = scan.nextLong();
-//		System.out.println("Your employee Id is "+empId);
-//      scan.close();
-//	}
-//}
+public class Scanner2{
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter your Employee Id:");
+		Long empId = scan.nextLong();
+		System.out.println("Your employee Id is "+empId);
+     scan.close();
+	}
+}
 
-//public class Scanner2{
-//	public static void main(String[] args) {
-//		Scanner scan = new Scanner(System.in);
-//		System.out.println("Enter your Employee Id :");
-//		Float empId = scan.nextFloat();
-//		System.out.println("Your Salary is:"+ empId);
-//		scan.close();
-//	}
-//}
+public class Scanner2{
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter your Employee Id :");
+		Float empId = scan.nextFloat();
+		System.out.println("Your Salary is:"+ empId);
+		scan.close();
+	}
+}
 
-//public class Scanner2{
-//	public static void main(String[] args) {
-//		Scanner scan = new Scanner(System.in);
-//		System.out.println("Enter your Salary:");
-//		Double salary = scan.nextDouble();
-//		System.out.println("your salary is "+ "$"+ salary );
-//	}
-//}
+public class Scanner2{
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter your Salary:");
+		Double salary = scan.nextDouble();
+		System.out.println("your salary is "+ "$"+ salary );
+	}
+}
 
 public class Scanner2{
 	public static void main(String[] args) {
