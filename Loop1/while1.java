042dce920f73d797e3d6757d7dde794c38ee2572
Sakshi Swarup <85953435+sakshiswarup@users.noreply.github.com>
2024-12-21 20:53:12 +0530

    Add files via upload

---
+++ Loop1/while1.java
@@ -0,0 +1,21 @@
+package loop;
+
+//public class while1 {
+//    public static void main(String[] args) {
+//        int x =0;
+//        while(x<5) {
+//            System.out.println(x);
+//            x++;
+//        }
+//    }
+//}
+
+public class while1{
+    public static void main(String[] args) {
+        int x =10;
+        while(x>0){
+            System.out.println(x);
+            x--;
+        }
+    }
+}
