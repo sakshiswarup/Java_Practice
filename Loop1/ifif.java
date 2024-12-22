75b51021081e78b3837cfdbcfe05096a5afb3373
Sakshi Swarup <85953435+sakshiswarup@users.noreply.github.com>
2024-12-22 22:09:17 +0530

    Add files via upload

---
+++ Loop1/ifif.java
@@ -0,0 +1,36 @@
+package loop;
+
+public class ifif{
+    public static void main(String[] args) {
+        int x=10;
+        if(x>5){
+            System.out.println("x is greater");
+        }else{
+            System.out.println("x is smaller");
+        }
+    }
+}
+
+//public class ifif{
+//    public static void main(String[] args) {
+//        x=10;
+//        if(x>50){
+//            System.out.println("x is greater");
+//        }else{
+//            System.out.println("x is smaller");
+//        }
+//    }
+//}
+
+//public class ifif{
+//    public static void main(String[] args) {
+//        int x= 10;
+//        if(x>50){
+//            System.out.println(1);
+//        }if(x>5){
+//            System.out.println(2);//o/p is 2,3
+//        }if(x>3){
+//            System.out.println(3);
+//        }
+//    }
+//}
