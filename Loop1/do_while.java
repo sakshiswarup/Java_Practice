aaff4053859796e3ebfc32a8ec655c71bfe88ca0
Sakshi Swarup <85953435+sakshiswarup@users.noreply.github.com>
2024-12-22 22:02:36 +0530

    Add files via upload

---
+++ Loop1/do_while.java
@@ -0,0 +1,11 @@
+package loop;
+//Do-while first it will run if condition is false but second iteration will not run if the second condition is false
+public class do_while {
+    public static void main(String[] args) {
+        int x =0;
+        do{
+            System.out.println(x);
+            x++;
+        }while (x<3);
+    }
+}
