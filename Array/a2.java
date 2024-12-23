f547118ffe88d6c647821756ba0aa0f230bb130e
Sakshi S. <85953435+sakshiswarup@users.noreply.github.com>
2024-12-23 22:15:43 +0530

    a2.java

--- Array/a2.java
+++ Array/a2.java
@@ -1,31 +1,31 @@
 package Array;
 
 
-//public class a2{
-//	public static void main(String[] args) {
-//		int []x=new int[3];
-//		x[0]=10;
-//		x[1]=20;
-//		x[2]=30;
-//		for(int i =0; i<x.length;i++) {
-//			System.out.println(x[i]);
-//		}
-//	}
-//}
+public class a2{
+	public static void main(String[] args) {
+		int []x=new int[3];
+		x[0]=10;
+		x[1]=20;
+		x[2]=30;
+		for(int i =0; i<x.length;i++) {
+			System.out.println(x[i]);
+		}
+	}
+}
 
 
-//public class a2 {
-//public static void main(String[] args) {
-//	String[] names= new String[3];
-//	names[0]="mike";
-//	names[1]="stallin";
-//	names[2]="joy";
-//	names[3]="sam";
-//	for(int i= 0; i<names.length;i++) {
-//		System.out.println(names[i]);//ArrayOutOfBound Exception.
-//	}
-//}
-//}
+public class a2 {
+public static void main(String[] args) {
+	String[] names= new String[3];
+	names[0]="mike";
+	names[1]="stallin";
+	names[2]="joy";
+	names[3]="sam";
+	for(int i= 0; i<names.length;i++) {
+		System.out.println(names[i]);//ArrayOutOfBound Exception.
+	}
+}
+}
 
 //for handle exception
 //public class a2{
