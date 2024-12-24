e68daf95e98280f19b8f7d13f288668521acc9ef
Sakshi S. <85953435+sakshiswarup@users.noreply.github.com>
2024-12-24 22:45:56 +0530

    F4.java

--- FileHandling/F4.java
+++ FileHandling/F4.java
@@ -3,20 +3,20 @@ package FileHandling;
 import java.io.FileWriter;
 
 //FileWriter()
-//public class F4 {
-//public static void main(String[] args) {
-//	try {
-//		FileWriter fw = new FileWriter("G:\\hello\\A.txt",true); //here append:true it means earlier data is there.
-//		//FileWriter fw = new FileWriter("G:\\hello\\A.txt",false);//here append is false so it means earlier data gone replace with new file
-//		fw.write(97); //it will change int to char
-//		fw.write("mike");
-//		fw.write("100");
-//		fw.close();
-//	} catch (Exception e) {
-//		e.printStackTrace();
-//	}
-//}
-//}
+public class F4 {
+public static void main(String[] args) {
+	try {
+		FileWriter fw = new FileWriter("G:\\hello\\A.txt",true); //here append:true it means earlier data is there.
+		//FileWriter fw = new FileWriter("G:\\hello\\A.txt",false);//here append is false so it means earlier data gone replace with new file
+		fw.write(97); //it will change int to char
+		fw.write("mike");
+		fw.write("100");
+		fw.close();
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+}
+}
 //other method
 
 public class F4{
@@ -34,18 +34,18 @@ public class F4{
 	}
 }
 
-//Examples
-//public class F4{
-//	public static void main(String[] args) {
-//		try {
-//			FileWriter fw = new FileWriter("G:\\hello\\A.txt",false);
-//			fw.write("Line1");
-//			fw.write("/n");
-//			fw.write("Line2");
-//			fw.write("/n");
-//			fw.close();
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
-//}
+// More Examples
+public class F4{
+	public static void main(String[] args) {
+		try {
+			FileWriter fw = new FileWriter("G:\\hello\\A.txt",false);
+			fw.write("Line1");
+			fw.write("/n");
+			fw.write("Line2");
+			fw.write("/n");
+			fw.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
