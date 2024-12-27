1c6a35b9665d7a30ffa4e1175e602db10dce22e3
Sakshi S. <85953435+sakshiswarup@users.noreply.github.com>
2024-12-27 21:31:54 +0530

    I1.java

--- Interface/I1.java
+++ Interface/I1.java
@@ -17,3 +17,18 @@ public interface I1{
 	public void test();
 	public void test2();
 }
+
+//3
+//Static members are not inherited in the below program  I2 .test() during compilation is converted to I1 .test() 
+//and I2.x is converted to I1.x .hence you get respective outputs.
+//public class I1{
+//	static int x =100;
+//	public static void test() {
+//		System.out.println(1);
+//	}
+//}
+
+//4
+public interface I1{
+public void test();
+}
