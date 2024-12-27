b2ee7367c3f8003511b888bf3702e87cb679b0e7
Sakshi S. <85953435+sakshiswarup@users.noreply.github.com>
2024-12-27 21:36:27 +0530

    F.java

--- Final/F.java
+++ Final/F.java
@@ -2,38 +2,38 @@ package Final;
 
 //Final Keyword- once you make a variable final then we cannot re-initialized that value.
 
-//public class F {
-//public static void main(String[] args) {
-//	final int x = 100;
-//	x=200;//error
-//	System.out.println(x);
-//}
-//}
+public class F {
+public static void main(String[] args) {
+	final int x = 100;
+	x=200;//error
+	System.out.println(x);
+}
+}
 
 //if you make static /non-static variables final then initialization is mandatory whenever it's give error.
-//public class F{
-//	final int x;//blank field error
-//	final static int y;//blank field error
-//	public static void main(String[] args) {
-//		
-//	}
-//}
+public class F{
+	final int x;//blank field error
+	final static int y;//blank field error
+	public static void main(String[] args) {
+		
+	}
+}
 
-//public class F{
-//	final int MAX;
-//	final static int MIN;//black field error (static & bold )
-//	public static void main(String[] args) {
-//		
-//	}
-//}
+public class F{
+	final int MAX;
+	final static int MIN;//black field error (static & bold )
+	public static void main(String[] args) {
+		
+	}
+}
 
 //if you make a method final then Overriding is not allowed
 
-//public class F{
-//	public final void test() {
-//		
-//	}
-//}
+public class F{
+	public final void test() {
+		
+	}
+}
 
 //if you make class final then inheritance is not allowed
 final public class F{
