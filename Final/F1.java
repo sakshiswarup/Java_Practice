8fbaf83a1125f3b0ab95cb1e5436f087262128bc
Sakshi S. <85953435+sakshiswarup@users.noreply.github.com>
2024-12-27 21:36:53 +0530

    F1.java

--- Final/F1.java
+++ Final/F1.java
@@ -1,25 +1,25 @@
 package Final;
 
-//public class F1 extends F {
-//	@Override
-//public void test() {// we cannot override final method (error)
-//	
-//}
-//}
+public class F1 extends F {
+	@Override
+public void test() {// we cannot override final method (error)
+	
+}
+}
 
 //public class F1 extends F{//error
 //}
 
-//Interface Continuation
-//Object of interface cannot be created.
-//Every variable in an interface by default final & static.
-//public interface F1{
-//	int MAX =100;
-//	int MIN=0;//even if i don't write final and static it will be final nd static(bold)
-//	public static void main(String[] args) {
-//		
-//	}
-//}
+Interface Continuation
+Object of interface cannot be created.
+Every variable in an interface by default final & static.
+public interface F1{
+	int MAX =100;
+	int MIN=0;//even if i don't write final and static it will be final nd static(bold)
+	public static void main(String[] args) {
+		
+	}
+}
 
 //we cannot create incomplete static method in an interface
 public interface F1{
