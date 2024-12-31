a7ada502666222523ef7bf3d0ec7298e57178c8f
Sakshi S. <85953435+sakshiswarup@users.noreply.github.com>
2024-12-31 22:44:55 +0530

    Add files via upload

---
+++ app_web_project/build/classes/com/app_web_project/model/DAOService.java
@@ -0,0 +1,47 @@
+package com.app_web_project.model;
+
+import java.sql.*;
+
+//DAO Service= (Data access object service) ,are responsible to perform database operations
+public class DAOService {
+
+	private Connection con;
+	private Statement stmnt;
+	
+	public void ConnectDB() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/app_project_db","root","test");
+			stmnt =con.createStatement();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public boolean verifyLogin(String email, String password) {
+		try {
+			ResultSet result = stmnt.executeQuery("select * from login where email='"+email+"' and password='"+password+"'");
+		  return result.next();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public void closeConnection() {
+		try {
+			con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void addStudent(String name, String email,String mobile) {
+		try {
+			stmnt.executeUpdate("insert into student values('"+name+"','"+email+"','"+mobile+"')");
+		} catch (Exception e) {
+		}
+	}
+	
+}
