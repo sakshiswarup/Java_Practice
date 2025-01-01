ef807fc79ca577a80d4930e5ac365d66905d06c6
Sakshi S. <85953435+sakshiswarup@users.noreply.github.com>
2025-01-01 21:45:29 +0530

    Add files via upload

---
+++ app_web_project/build/classes/com/app_web_project/controller/LoginController.java
@@ -0,0 +1,48 @@
+package com.app_web_project.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.app_web_project.model.DAOService;
+
+
+@WebServlet("/verifyLogin")
+public class LoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+  
+    public LoginController() {
+        super();
+        
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		
+		DAOService service = new DAOService();
+		service.ConnectDB();
+		
+		boolean status = service.verifyLogin(email, password);
+		if(status) {
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/student_registration.jsp");
+			rd.forward(request, response);
+		}else {
+			request.setAttribute("error", "Invalid username/password");
+			RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+			rd.forward(request, response);
+		}
+		
+	}
+		
+}
