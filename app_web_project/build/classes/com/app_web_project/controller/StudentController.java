ff7ef57226a6cc73376825a223807654c3a341f6
Sakshi S. <85953435+sakshiswarup@users.noreply.github.com>
2025-01-02 21:35:22 +0530

    StudentController.java

--- app_web_project/build/classes/com/app_web_project/controller/StudentController.java
+++ app_web_project/build/classes/com/app_web_project/controller/StudentController.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.app_web_project.model.DAOService;
 
@@ -24,22 +25,37 @@ public class StudentController extends HttpServlet {
 
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/student_registration.jsp");
+		rd.forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String name = request.getParameter("name");
-		String email = request.getParameter("email");
-		String mobile = request.getParameter("mobile");
+		try {
+			HttpSession session = request.getSession(false);
+			//sessionTimeout
+			session.setMaxInactiveInterval(10);
+			if(session.getAttribute("email")!=null) {
+				String name = request.getParameter("name");
+				String email = request.getParameter("email");
+				String mobile = request.getParameter("mobile");
+				
+				DAOService service = new DAOService();
+				service.ConnectDB();
+				service.addStudent(name, email, mobile);
+				
+				//form blank page to student_registration.jsp
+				request.setAttribute("msg","Student is now added..");
+				RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/student_registration.jsp");
+				rd.forward(request, response);
+			}else {
+				RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+				rd.forward(request, response);
+			}
+		} catch (Exception e) {
+			RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+			rd.forward(request, response);
+		}
 		
-		DAOService service = new DAOService();
-		service.ConnectDB();
-		service.addStudent(name, email, mobile);
-		
-		//form blank page to student_registration.jsp
-		request.setAttribute("msg","Student is now added..");
-		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/student_registration.jsp");
-		rd.forward(request, response);
 	}
 
 }
