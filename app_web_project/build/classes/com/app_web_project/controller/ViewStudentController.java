5110757722ea206781812e340429d800d92861bc
Sakshi S. <85953435+sakshiswarup@users.noreply.github.com>
2025-01-02 21:36:28 +0530

    ViewStudentController.java

--- app_web_project/build/classes/com/app_web_project/controller/ViewStudentController.java
+++ app_web_project/build/classes/com/app_web_project/controller/ViewStudentController.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.app_web_project.model.DAOService;
 
@@ -24,17 +25,31 @@ public class ViewStudentController extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		DAOService service = new DAOService();
-		service.ConnectDB();
-		
-		ResultSet result=service.getAllStudents();
-		
-		request.setAttribute("result",result);
-		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/list_registration.jsp");
-		rd.forward(request,response);
+		try {
+			
+			HttpSession session = request.getSession(false);
+			//sessionTimeout
+			session.setMaxInactiveInterval(10);
+			if(session.getAttribute("email")!=null) {
+				DAOService service = new DAOService();
+				service.ConnectDB();
+				
+				ResultSet result=service.getAllStudents();
+				
+				request.setAttribute("result",result);
+				RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/list_registration.jsp");
+				rd.forward(request,response);
+				}
+			else {
+				RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+				rd.forward(request,response);
+			}
+		} catch (Exception e) {
+			RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+			rd.forward(request,response);
 		}
-	
-
+		
+	}	
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
