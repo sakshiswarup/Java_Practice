6ac0aae4a57d5228910b40bcc40515f93e3848fb
Sakshi S. <85953435+sakshiswarup@users.noreply.github.com>
2025-01-02 21:38:11 +0530

    Add files via upload

---
+++ app_web_project/build/classes/com/app_web_project/controller/logoutController.java
@@ -0,0 +1,37 @@
+package com.app_web_project.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/logoutController")
+public class logoutController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public logoutController() {
+        super();
+       
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+		rd.forward(request, response);
+		
+		//Distroy session object
+		HttpSession session = request.getSession(false);
+		session.invalidate();
+	}
+
+}
