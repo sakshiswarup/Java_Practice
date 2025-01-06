e9761ffe38401329aae9a3d9f52691cd2f16a134
Sakshi S. <85953435+sakshiswarup@users.noreply.github.com>
2025-01-06 20:46:15 +0530

    DemoApplicationTests.java

--- demo/src/test/java/com/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/demo/DemoApplicationTests.java
@@ -3,6 +3,8 @@ package com.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,9 +21,9 @@ class DemoApplicationTests {
 	@Test
 	public void createStudentRegistration() {
 		Student s = new Student();
-		s.setId(1L);
-		s.setName("mike");
-		s.setEmail("mike@gmail.com");
+		s.setId(3L);
+		s.setName("stallin");
+		s.setEmail("stallin@gmail.com");
 		s.setMobile("9632629033");
 		Student student = service.createStudent(s);
 		
@@ -31,7 +33,42 @@ class DemoApplicationTests {
 		System.out.println(student.getMobile());
 		assertEquals(s.getMobile(),student.getMobile());
 	}
-
+    
+   @Test
+   void deleteStudent() {
+	   //J unit testing 
+	 // boolean actualValue = service.deleteStudentById(10L);
+	   boolean actualValue = service.deleteStudentById(1L);
+	  boolean expectedValue = false;
+	  assertEquals(expectedValue, actualValue);
+	  
+   }
 	
-	
+   @Test
+   void findStudentById() {
+	   boolean actualValue =service.findStudentById(1L);
+	 //testing purpose  boolean actualValue = service.findStudentById(10L);
+	   boolean expectedValue =true;
+	   assertEquals(expectedValue,actualValue,"messege");
+	   //testing purpose assertEquals(expectedValue,actualValue,"record not found");	   
+	   
+   }
+   
+   @Test
+   void updateStudentRecord() {
+	   Student updatedRecord = service.updateStudent();
+	   assertEquals(1L,updatedRecord.getId(),"Id was not found in database");
+	  // assertEquals("stallin",updatedRecord.getName(),"Name was not updated");
+   }
+   @Test
+   void getAllRecords() {
+	// service.getAllRecords();
+	  Iterable<Student> allRecords= service.getAllRecords();
+	  for(Student s:allRecords) {
+		  System.out.println(s.getId());
+		  System.out.println(s.getName());
+		  System.out.println(s.getEmail());
+		  System.out.println(s.getMobile());
+	  }
+   }
 }
