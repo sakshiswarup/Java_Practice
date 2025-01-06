065abaedef96acd7d652d6698db6a756a4c593eb
Sakshi S. <85953435+sakshiswarup@users.noreply.github.com>
2025-01-06 20:47:07 +0530

    StudentService.java

--- demo/src/main/java/com/demo/service/StudentService.java
+++ demo/src/main/java/com/demo/service/StudentService.java
@@ -1,5 +1,7 @@
 package com.demo.service;
 
+
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,7 +10,7 @@ import com.demo.repository.StudentRepository;
 
 @Service
 public class StudentService {
-  
+	@Autowired
 	private StudentRepository repository;
 	//write Create Student Registration
 	
@@ -16,4 +18,50 @@ public class StudentService {
 		Student s = repository.save(student);
 		return s;
 	}
+//	@Test
+//	void deleteStudent() {
+//		Optional<Student> opStudent =repository.findById(1L);
+//		if(opStudent.isPresent()) {
+//			System.out.println("Present");
+//		}else {
+//			System.out.println("Null reference");
+//		}
+//	}
+	
+	public boolean deleteStudentById(Long id) {
+		repository.deleteById(id);
+		Optional<Student> opStudent = repository.findById(id);
+		//for j unit testing-Optional<Student> opStudent = repository.findById(1L);
+		if(opStudent.isPresent()) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+
+	public boolean findStudentById(long id) {
+		Optional<Student> opStudent = repository.findById(id);
+		if(opStudent.isPresent()) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+
+	public Student updateStudent() {
+		Student s = new Student();
+		s.setId(1L);
+		s.setName("mike");
+		s.setEmail("mike@gmail.com");
+		s.setMobile("9632629455");
+		return repository.save(s);
+	}
+
+	public Iterable<Student> getAllRecords() {
+//		Iterable<Student> itrStudent = repository.findAll();//iterable- collection of object present in it
+//		System.out.println(itrStudent);
+		
+		return repository.findAll();
+	}
+	
 }
