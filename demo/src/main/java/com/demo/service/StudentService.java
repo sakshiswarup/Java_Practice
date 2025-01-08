55c02eff15c30862feea11c291c0677ee5e07958
Sakshi S. <85953435+sakshiswarup@users.noreply.github.com>
2025-01-08 11:53:13 +0530

    StudentService.java

--- demo/src/main/java/com/demo/service/StudentService.java
+++ demo/src/main/java/com/demo/service/StudentService.java
@@ -58,10 +58,55 @@ public class StudentService {
 	}
 
 	public Iterable<Student> getAllRecords() {
-//		Iterable<Student> itrStudent = repository.findAll();//iterable- collection of object present in it
+//		Iterable<Student> itrStudent = repository.findAll();//Iterable<>- collection of object present in it
 //		System.out.println(itrStudent);
 		
 		return repository.findAll();
 	}
 	
-}
+	//find by email with the use of finder method
+	public Student getStudentByEmail() {
+		Optional<Student> opStudent= repository.findByEmail("mike@gmail.com");
+		if(opStudent.isPresent()) {
+			Student student = opStudent.get();
+			return student;
+		}
+		return null;
+		}
+	
+	public Student getStudentByMobile() {
+		Optional<Student> opStudent = repository.findByMobile("9632629455");
+		if(opStudent.isPresent()) {
+			Student student = opStudent.get();
+			return student;
+		}
+		return null;
+	}
+	
+	public Student getStudentByEmailAndMobile() {
+		Optional<Student> opStudent = repository.findByEmailAndMobile("mike@gmail.com", "9632629033");
+		if(opStudent.isPresent()) {
+			Student student=opStudent.get();
+			return student;
+		}
+		return null;
+	}
+	public Iterable<Student> getByEmailOrMobile(){
+		return repository.findByEmailOrMobile("ram@gmail.com", "9632629455");
+	}
+	
+	public Student searchStudentByEmail() {
+		Student student = repository.searchByEmail("mike@gmail.com");
+		return student;
+	}
+	
+	public Student searchStudentByMobile() {
+		Student student = repository.searchByMobile("9632629455");
+		return student;
+	}
+	
+	public Student searchStudentByEmailAndMobile() {
+	Student student = repository.searchByEmailAndMobile("mike@gmail.com", "9632629455");
+		return student;
+	}
+	}
