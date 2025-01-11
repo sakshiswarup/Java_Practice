304f3d9e60628a1d4662abef4028018575079d2d
Sakshi S. <85953435+sakshiswarup@users.noreply.github.com>
2025-01-11 20:46:14 +0530

    Add files via upload

---
+++ String_Concepts/S2.java
@@ -0,0 +1,31 @@
+package String_Concepts;
+//trim()
+//public class S2 {
+//public static void main(String[] args) {
+//	String x1=" aab ";
+//	String x2="\n\t Acc";
+//	System.out.println(x1.trim());//o/p=aab
+//	System.out.println(x2.trim());//Acc
+//}
+//}
+
+//charAt()
+//public class S2{
+//	public static void main(String[] args) {
+//	String x1="aabaacv";
+//	System.out.println(x1.charAt(0));
+//	}
+//}
+
+public class S2{
+	public static void main(String[] args) {
+		String x1="aabaacv";
+		int count=0;
+		for(int i =0; i<x1.length();i++) {
+			if(x1.charAt(i)=='a') {
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+}
