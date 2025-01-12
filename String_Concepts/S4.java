5d4c40f17c72244d38fc6e5eedadd25ebd201b07
Sakshi S. <85953435+sakshiswarup@users.noreply.github.com>
2025-01-12 14:36:01 +0530

    Add files via upload

---
+++ String_Concepts/S4.java
@@ -0,0 +1,37 @@
+package String_Concepts;
+
+//join()
+//public class S4 {
+//public static void main(String[] args) {
+//	String [] words= {"hello","world","java","programming"};
+//	//String str=String.join(",", words);
+//	String str = String.join("-", words);
+//	System.out.println(str);
+//}
+//}
+
+//replace()
+//public class S4{
+//	public static void main(String[] args) {
+//		String s ="hello world";
+//		String str=s.replace("world", "java");
+//		System.out.println(str);
+//	}
+//}
+
+//public class S4{
+//	public static void main(String[] args) {
+//		String s ="Hello world";
+//		String str = s.replace("l", "*");
+//		System.out.println(str);
+//	}
+//}
+
+//concat()
+public class S4{
+	public static void main(String[] args) {
+		String s1="hello";
+		String s2="world";
+		System.out.println(s1.concat(" ").concat(s2));
+	}
+}
