39d0502c74dcde5050edd6cd7ba3b99b9fb352e7
Sakshi S. <85953435+sakshiswarup@users.noreply.github.com>
2025-01-12 14:35:37 +0530

    Add files via upload

---
+++ String_Concepts/S3.java
@@ -0,0 +1,49 @@
+package String_Concepts;
+//convert primitive data type to string.
+//public class S3 {
+//public static void main(String[] args) {
+//	int x = 20;
+//	String s1=String.valueOf(x);
+//	float y =10.1f;
+//	String s2=String.valueOf(y);
+//	char z='a';
+//	String s3=String.valueOf(z);
+//	boolean m = true;
+//	String s4=String.valueOf(m);
+//	System.out.println(s1);
+//	System.out.println(s2);
+//	System.out.println(s3);
+//	System.out.println(s4);
+//}
+//}
+
+//indexOf()= Return the position of the first found occurrence of specified character in a string
+//public class S3{
+//	public static void main(String[] args) {
+//		String s = "hello world where are you";
+//		//System.out.println(s.indexOf("world"));//6
+//		System.out.println(s.indexOf("w"));//6
+//		System.out.println(s.indexOf("W"));//-1
+//		System.out.println(s.indexOf("N"));//-1
+//		System.out.println(s.indexOf("w",7));//12
+//		System.out.println(s.indexOf("o",2));
+//	}
+//}
+
+//How to find sub String in the given String
+
+public class S3{
+	public static void main(String[] args) {
+		String s ="hi my name is mike";
+		System.out.println(s.contains("mike"));//true
+	}
+}
+
+
+//public class S3{
+//	public static void main(String[] args) {
+//		String s="hi my name is mike";
+//		System.out.println(s.substring(14,18));//mike
+//	}
+//}
+//
