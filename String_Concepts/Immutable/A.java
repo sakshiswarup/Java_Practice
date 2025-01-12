c2837a4d06055f5bbb492059c5c481683a62c65e
Sakshi S. <85953435+sakshiswarup@users.noreply.github.com>
2025-01-12 14:36:31 +0530

    Add files via upload

---
+++ String_Concepts/Immutable/A.java
@@ -0,0 +1,15 @@
+package String_Concepts.Immutable;
+//Immutable class
+
+
+public final class A {
+private final int x;
+A(int x){
+	this.x=x;
+}
+//getters
+public int getX() {
+	return x;
+}
+
+}
---
+++ String_Concepts/Immutable/B.java
@@ -0,0 +1,8 @@
+package String_Concepts.Immutable;
+
+public class B {
+public static void main(String[] args) {
+	A a1= new A(10);
+	System.out.println(a1.getX());
+}
+}
