7830ea663a56b3b060e4c4552362f564e8efe8ff
Sakshi S. <85953435+sakshiswarup@users.noreply.github.com>
2025-01-15 21:53:38 +0530

    A.java

--- Abstract/A.java
+++ Abstract/A.java
@@ -1,25 +1,25 @@
 package Abstract;
 
 //Abstract in java= An abstract class can consist of both complete and incomplete method in it.
-//abstract public class A {
-// abstract public void test1();
-//  public void test2() {
-//	System.out.println(2);
-//}
-//}
+abstract public class A {
+abstract public void test1();
+ public void test2() {
+	System.out.println(2);
+}
+}
 
 //Abstract  class can consists of both static and non static methods/variable.
 
-//abstract public class A{
-//	int x =0;
-//	static int y = 20;
-//}
+abstract public class A{
+	int x =0;
+	static int y = 20;
+}
 
 //An object of abstract class cannot be created.
-//abstract public class A{
-//	int x = 10;
-//	static int y = 20;
-//}
+abstract public class A{
+	int x = 10;
+	static int y = 20;
+}
 
 //Abstract class does not class multiple inheritance.
 //abstract public class A{
