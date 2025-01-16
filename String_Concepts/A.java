a1227487f8ab9105918068cf3ca7e9185d4edb38
Sakshi S. <85953435+sakshiswarup@users.noreply.github.com>
2025-01-16 13:11:05 +0530

    Add files via upload

---
+++ String_Concepts/A.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class A {
+    public static void main(String[] args) {
+        List<String> arr =new ArrayList<>();
+        arr.add("Apple");
+        arr.add("Banana");
+        arr.add("Cherry");
+
+//        Iterator<String> val = arr.iterator();
+//        while(val.hasNext()){//if value is check in next position then print nd move pointer to next
+//            String x = val.next();
+//            System.out.println(x);
+//        }
+        //or another way to iterate values
+        for(String x: arr){
+            System.out.println(x);
+        }
+    }
+}
