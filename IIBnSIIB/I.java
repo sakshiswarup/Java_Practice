b4daffbd327e862f4607acc6dc2d4f465056d8bd
Sakshi S. <85953435+sakshiswarup@users.noreply.github.com>
2025-01-18 21:12:11 +0530

    I.java

--- IIBnSIIB/I.java
+++ IIBnSIIB/I.java
@@ -3,15 +3,15 @@ package IIBnSIIB;
 //IIB's are executed when objects are created
 //no of times we create object same number of times IIB called.
 //IIB are used to initialized all the instance variable in one place that gives us better readability of code. 
-//public class I {
-//	{
-//		System.out.println("From IIB");
-//	}
-//	public static void main(String[] args) {
-//		I i = new I();
-//		I i1= new I();//o/p from IIB from IIB
-//	}
-//}
+public class I {
+	{
+		System.out.println("From IIB");
+	}
+	public static void main(String[] args) {
+		I i = new I();
+		I i1= new I();//o/p from IIB from IIB
+	}
+}
 
 
 //public class I{//o/p  Start from main from IIb from constructor from main
