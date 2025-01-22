8c10d099f77392a57cb61c5e673556a620d585de
Sakshi S. <85953435+sakshiswarup@users.noreply.github.com>
2025-01-22 22:25:15 +0530

    RegistrationService.java

--- webdemo/src/main/java/com/webdemo/service/RegistrationService.java
+++ webdemo/src/main/java/com/webdemo/service/RegistrationService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.webdemo.dto.RegistrationDto;
 import com.webdemo.entity.Registration;
 import com.webdemo.repository.RegistrationRepository;
 
@@ -21,6 +22,23 @@ private RegistrationRepository registrationRepository;
 		List<Registration> registrations = registrationRepository.findAll();
 		//findAll( return type is iterable(in crud repository) but here we use jpa repository so return type is List
 		return registrations;
+	}
+
+	public void deleteRegistration(long id) {
+		registrationRepository.deleteById(id);
+		
+	}
+
+	public Registration getRegistration(long id) {
+	return registrationRepository.findById(id).get();	
+	}
+
+	public void updateRegistration(RegistrationDto dto) {
+		Registration registration = registrationRepository.findById(dto.getId()).get();
+		registration.setName(dto.getName());
+		registration.setEmailId(dto.getEmailId());
+		registration.setMobile(dto.getMobile());
+		registrationRepository.save(registration);
 	}
 
 }
