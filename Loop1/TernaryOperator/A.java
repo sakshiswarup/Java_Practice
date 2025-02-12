9955325655d4fb76d7975c87da1ff1156c2bb92f
Sakshi S. <85953435+sakshiswarup@users.noreply.github.com>
2025-02-12 21:24:46 +0530

    Add files via upload

---
+++ Loop1/TernaryOperator/A.java
@@ -0,0 +1,10 @@
+package TernaryOperator;
+
+//ternary operator alternative to write if else
+public class A {
+    public static void main(String[] args) {
+        int x = 10;
+        String val= x>10 ? "x is greater" :"xis not greater";
+        System.out.println(val);
+    }
+}
