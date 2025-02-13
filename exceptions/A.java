76a1d08506bfd46b7b121aaa631a8c86e9696b47
Sakshi S. <85953435+sakshiswarup@users.noreply.github.com>
2025-02-13 21:17:39 +0530

    A.java

--- exceptions/A.java
+++ exceptions/A.java
@@ -3,24 +3,24 @@ package exceptions;
 //we can handle exception in java using try catch block
 //when an exception occurs in try catch block, try block will create exception object and 
 //this objects address it will give it to catch block. Catch block will now suppress the exception & hence further will continue to run.
-//public class A {
-// public static void main(String[] args) {
-//	int x = 10;
-//	int y = 0;
-//	int z = x/y;
+public class A {
+ public static void main(String[] args) {
+	int x = 10;
+	int y = 0;
+	int z = x/y;
 	// System.out.println(z);//it will give an ArithmeicException.
-//	System.out.println("welcome");
-//	 try {
-//			int x = 10;
-//			int y = 0;
-//			int z = x/y;
-//			System.out.println(z);
-//	} catch (Exception e) {//here e is reference variable
-//		System.out.println(e);//print reference address.
-//	}
-//	 System.out.println("welcome");
-//}
-//}
+	System.out.println("welcome");
+	 try {
+			int x = 10;
+			int y = 0;
+			int z = x/y;
+			System.out.println(z);
+	} catch (Exception e) {//here e is reference variable
+		System.out.println(e);//print reference address.
+	}
+	 System.out.println("welcome");
+}
+}
 
 public class A{
 	public static void main(String[] args) {
