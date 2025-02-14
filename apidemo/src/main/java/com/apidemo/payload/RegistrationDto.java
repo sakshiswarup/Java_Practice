70b1e1f283c5d07bfcdb1ec931f72b0ac85fdea6
Sakshi S. <85953435+sakshiswarup@users.noreply.github.com>
2025-02-14 21:14:53 +0530

    RegistrationDto.java

--- apidemo/src/main/java/com/apidemo/payload/RegistrationDto.java
+++ apidemo/src/main/java/com/apidemo/payload/RegistrationDto.java
@@ -1,5 +1,8 @@
 package com.apidemo.payload;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,8 +10,13 @@ import lombok.Setter;
 //@Setter
 public class RegistrationDto {
     private long id;
+    @NotNull(message = "Can not be null")
+    @Size(min=2, max = 10, message = "Should be between 2 to 10 cjharacters")
     private String name;
+    @Email(message = "Invalid email address")
     private String emailId;
+    @NotNull(message = "Cannot be null")
+    @Size(min = 10 , max = 10, message = "should be exactly 10 digits")
     private String mobile;
 
     public long getId() {
